f51dc6d78e29a196c774777246ad729ceb27358f
Akki710 <Akshaysinghswarnkar@gmail.com>
2022-03-09 21:16:17 +0530

    ECOM Backend

---
+++ app.js
@@ -0,0 +1,30 @@
+// Imports
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+// Import routes
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+// Calling express method
+const app = express();
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// DB connection
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("Database CONNECTED"))
+  .catch((err) => console.log("Error occured : ", err));
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+// Routes
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+// Listening to port
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
---
+++ models/product.js
@@ -0,0 +1,47 @@
+// Imports
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
+
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 200,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 2000,
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 30,
+      trim: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      rate: {
+        type: Number,
+      },
+      count: {
+        type: Number,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
