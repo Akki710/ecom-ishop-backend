76a34e9821886c5b1a0dda31627aa8e2314b78db
Akki710 <Akshaysinghswarnkar@gmail.com>
2022-03-09 23:24:52 +0530

    ECOM Backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Imports
-require('dotenv').config();
+var dotenv=require('dotenv');
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -11,9 +12,10 @@ const app = express();
 
 // Port
 const PORT = process.env.PORT || 5000;
+const uri='mongodb+srv://akki710:@test-cluster.frzty.mongodb.net/EcomDB?retryWrites=true&w=majority'
 
 // DB connection
-mongoose.connect(process.env.DB ,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect( process.env.DB ,{useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("Database CONNECTED"))
   .catch((err) => console.log("Error occured : ", err));
 
